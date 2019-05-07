6392c33a2c77a90c9f14ccf1798a641970d24aba
Ivan Francisco Prieto <ifr.prieto@gmail.com>
2019-05-07 13:21:21 -0500

    Basic OWebhook app

--- src/OrishikuDotCom/settings/site.py
+++ src/OrishikuDotCom/settings/site.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'secret-key-site'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['orishiku.com', 'www.orishiku.com']
 
 # Application definition
 
@@ -33,3 +33,14 @@ SITE_ID = 1
 
 # OPages 
 DEFAULT_TEMPLATE = 'Pages/default.html'
+if not DEBUG:
+    SECURE_PROXY_SSL_HEADER        = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT            = True
+    SESSION_COOKIE_SECURE          = True
+    CSRF_COOKIE_SECURE             = True
+    SECURE_HSTS_SECONDS            = True
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_CONTENT_TYPE_NOSNIFF    = True
+    SECURE_BROWSER_XSS_FILTER      = True
+    X_FRAME_OPTIONS                = 'DENY'
+    SECURE_HSTS_PRELOAD            = True
